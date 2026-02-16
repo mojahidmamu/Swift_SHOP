3444f8b3614d57912740a0cd578cdcabcd17fe4d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-16 11:35:16 +0600

    added

--- logic.js
+++ logic.js
@@ -1,41 +1,60 @@
-const productGrid = document.getElementById("productGrid");
+const productGrid = document.getElementById("productContainer");
 const cartCount = document.getElementById("cartCount");
 
 let cart = 0;
 
-// fetch top 3 products
+// Top rated 3 products card 
 fetch("https://fakestoreapi.com/products")
-  .then(res => res.json())
-  .then(data => {
-
+  .then((res) => res.json())
+  .then((data) => {
     const topProducts = data
       .sort((a, b) => b.rating.rate - a.rating.rate)
       .slice(0, 3);
 
-    topProducts.forEach(p => {
+    productGrid.innerHTML = "";
 
+    topProducts.forEach((p) => {
       const card = document.createElement("div");
-      card.className = "bg-white p-6 rounded shadow";
+      card.className = "bg-white p-6 rounded-lg shadow-md flex flex-col mb-8";
 
       card.innerHTML = `
         <img src="${p.image}" class="h-48 mx-auto object-contain mb-4">
-        <h3 class="font-semibold mb-2 line-clamp-2">${p.title}</h3>
-        <p class="text-indigo-600 font-bold mb-2">$${p.price}</p>
-        <p class="text-sm text-gray-500 mb-4">⭐ ${p.rating.rate}</p>
 
-        <button class="addBtn bg-indigo-600 text-white px-4 py-2 rounded w-full">
+        <p class="text-sm text-gray-500 mb-4">
+          ⭐ ${p.rating.rate} (${p.rating.count})
+        </p>
+
+        <h3 class="font-semibold mb-2">
+          ${p.title}
+        </h3>
+
+        <p class="text-indigo-600 font-bold mb-1">
+          $${p.price}
+        </p>
+
+      <div class="flex gap-2">
+        <button class="text-black border rounded-lg px-4 py-2 ">
+          View Details
+        </button>
+        <button class="addBtn mt-auto bg-indigo-600 text-white px-4 py-2 rounded-lg hover:bg-indigo-700">
           Add To Cart
         </button>
+      </div>
       `;
 
       productGrid.appendChild(card);
     });
 
-    document.querySelectorAll(".addBtn").forEach(btn => {
+    // cart button events
+    document.querySelectorAll(".addBtn").forEach((btn) => {
       btn.addEventListener("click", () => {
         cart++;
         cartCount.innerText = cart;
       });
     });
-
+  })
+  .catch((err) => {
+    productGrid.innerHTML =
+      "<p class='text-red-500'>Failed to load products</p>";
+    console.error(err);
   });
